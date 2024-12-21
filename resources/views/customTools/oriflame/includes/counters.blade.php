cb629ca7a1ef8a67c9753ea1f0be56ce1bb2c1be
Bruno <wl_avv@hotmail.com>
2024-12-21 16:16:43 +0000

    Update counters.blade.php

--- resources/views/customTools/oriflame/includes/counters.blade.php
+++ resources/views/customTools/oriflame/includes/counters.blade.php
@@ -3,8 +3,8 @@
         <div class="row">
             <div class="col-lg-2 col-md-3 col-sm-6 col-6">
                 <div class="card-counter primary">
-                    <div style="width: 100px;float: left;"> <i class="fa fa-code-fork"></i> </div>
-                    <div style="width: calc( 100% - 100px); float: left;padding: 10px 0;">
+                    <div style="width: 80px;float: left;"> <i class="fa fa-code-fork"></i> </div>
+                    <div style="width: calc( 100% - 80px); float: left;padding: 10px 0;">
                         <div class="count-numbers">17</div>
                         <br><div class="count-name">Orders</div>
                     </div>
@@ -12,8 +12,8 @@
             </div>
             <div class="col-lg-2 col-md-3 col-sm-6 col-6">
                 <div class="card-counter info">
-                    <div style="width: 100px;float: left;"> <i class="fa fa-boxes-packing"></i> </div>
-                    <div style="width: calc( 100% - 100px); float: left;padding: 10px 0;">
+                    <div style="width: 80px;float: left;"> <i class="fa fa-boxes-packing"></i> </div>
+                    <div style="width: calc( 80% - 100px); float: left;padding: 10px 0;">
                         <span class="count-numbers">12</span>
                         <br><span class="count-name">Open</span>
                     </div>
@@ -21,8 +21,8 @@
             </div>
             <div class="col-lg-2 col-md-3 col-sm-6 col-6">
                 <div class="card-counter success">
-                    <div style="width: 100px;float: left;"> <i class="fa fa-truck-fast"></i> </div>
-                    <div style="width: calc( 100% - 100px); float: left;padding: 10px 0;">
+                    <div style="width: 80px;float: left;"> <i class="fa fa-truck-fast"></i> </div>
+                    <div style="width: calc( 80% - 100px); float: left;padding: 10px 0;">
                         <span class="count-numbers">2312</span>
                         <br><span class="count-name">Shipped</span>
                     </div>
@@ -30,8 +30,8 @@
             </div>
             <div class="col-lg-2 col-md-3 col-sm-6 col-6">
                 <div class="card-counter danger">
-                    <div style="width: 100px;float: left;"> <i class="fa fa-xmark"></i> </div>
-                    <div style="width: calc( 100% - 100px); float: left;padding: 10px 0;">
+                    <div style="width: 80px;float: left;"> <i class="fa fa-xmark"></i> </div>
+                    <div style="width: calc( 100% - 80px); float: left;padding: 10px 0;">
                         <span class="count-numbers">104</span>
                         <br><span class="count-name">Cancelled</span>
                     </div>
