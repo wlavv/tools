55d011d09e4a9bd335f7e69d536b7fefd56d5fd8
Bruno <wl_avv@hotmail.com>
2025-05-01 19:35:01 +0100

    Update find.blade.php

--- resources/views/mtg/front/find.blade.php
+++ resources/views/mtg/front/find.blade.php
@@ -8,6 +8,9 @@
         <script src="https://cdn.jsdelivr.net/npm/@tensorflow-models/coco-ssd"></script>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
+        <!-- Adiciona o OpenCV.js via CDN -->
+<script async src="https://docs.opencv.org/master/opencv.js" type="text/javascript"></script>
+
         @include("mtg.front.includes.css")
     </head>
     <body>
