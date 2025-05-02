7104e04716535fb681f8ca205dd7039610d557d7
Bruno <wl_avv@hotmail.com>
2025-05-02 11:28:26 +0100

    Update find.blade.php

--- resources/views/mtg/front/find.blade.php
+++ resources/views/mtg/front/find.blade.php
@@ -5,13 +5,13 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>MTG Card Tracker with pHash</title>
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.4.0/p5.js"></script>
         @include("mtg.front.includes.css")
     </head>
     <body>
         <div class="container">
             <h1>Tracking de Objetos com Webcam</h1>
             <video id="video" width="640" height="480" autoplay></video>
+
             <button id="startTracking" onclick="startTracking()">Iniciar Tracking</button>
             <div class="controls">
                 <div class="control-group">
