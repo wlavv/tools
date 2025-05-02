4a1a89f888f7656b3258d431bfb0868ef35f84fa
Bruno <wl_avv@hotmail.com>
2025-05-02 16:28:03 +0100

    Update find.blade.php

--- resources/views/mtg/front/find.blade.php
+++ resources/views/mtg/front/find.blade.php
@@ -1,158 +1,91 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Magic: The Gathering AR - Cloudpost</title>
-    <script src="https://aframe.io/releases/1.5.0/aframe.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/mind-ar@1.2.5/dist/mindar-image-aframe.prod.js"></script>
-    <style>
-        body {
-            margin: 0;
-            overflow: hidden;
-            background-color: #1e1e1e;
-        }
-        .example-container {
-            position: relative;
-            width: 100%;
-            height: 100vh;
-        }
+<html>
+   <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta aframe-injected="" name="mobile-web-app-capable" content="yes">
+      <meta aframe-injected="" name="theme-color" content="black">
+      @include("mtg.front.includes.css")
+      @include("mtg.front.includes.js")
+    </head>
+    <body cz-shortcut-listen="true">
+        <div class="example-container">
+            <div id="example-scanning-overlay" class="">
+                <div class="inner">
+                    <img src="/images/mtg/custom_images/Magic_card_back.png">
+                    <div class="scanline"></div>
+                </div>
+            </div>
 
-        /* Animação de scan */
-        #example-scanning-overlay {
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: rgba(0, 0, 0, 0.6);
-        }
-        .scanline {
-            position: absolute;
-            width: 100%;
-            height: 3px;
-            background: rgba(255, 255, 255, 0.7);
-            animation: scan 1s linear infinite;
-            top: 50%;
-        }
-        @keyframes scan {
-            0% { top: -10%; }
-            100% { top: 110%; }
-        }
+            <a-scene mindar-image="imageTargetSrc: /images/mtg/minds/cloudpost.mind; showStats: false; uiScanning: #example-scanning-overlay;" embedded="" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: false" device-orientation-permission-ui="enabled: false" inspector="" keyboard-shortcuts="" screenshot="">
+                <a-assets>
+                    <img id="card" src="./assets/card-example/card.png">
+                    <img id="icon-web" src="./assets/card-example/icons/web.png">
+                    <img id="icon-location" src="./assets/card-example/icons/location.png">
+                    <img id="icon-profile" src="./assets/card-example/icons/profile.png">
+                    <img id="icon-phone" src="./assets/card-example/icons/phone.png">
+                    <img id="icon-email" src="./assets/card-example/icons/email.png">
+                    <img id="icon-play" src="./assets/card-example/icons/play.png">
+                    <img id="icon-left" src="./assets/card-example/icons/left.png">
+                    <img id="icon-right" src="./assets/card-example/icons/right.png">
+                    <img id="paintandquest-preview" src="./assets/card-example/portfolio/paintandquest-preview.png">
+                    <video id="paintandquest-video-mp4" autoplay="false" loop="true" src="./assets/card-example/portfolio/paintandquest.mp4" playsinline="" webkit-playsinline=""></video>
+                    <video id="paintandquest-video-webm" autoplay="false" loop="true" src="./assets/card-example/portfolio/paintandquest.webm" playsinline="" webkit-playsinline=""></video>
+                    <img id="coffeemachine-preview" src="./assets/card-example/portfolio/coffeemachine-preview.png">
+                    <img id="peak-preview" src="./assets/card-example/portfolio/peak-preview.png">
+                    <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.2/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
+                </a-assets>
+                <a-camera position="0 0 0" look-controls="enabled: false" cursor="fuse: false; rayOrigin: mouse;" raycaster="far: 10000; objects: .clickable" camera="" rotation="" wasd-controls=""></a-camera>
+                                
+                <a-entity id="mytarget" mytarget="" mindar-image-target="targetIndex: 0">
 
-        /* Painéis de Glass UI - Estilo */
-        .glass-panel {
-            position: absolute;
-            width: 30%;
-            height: 40%;
-            background: rgba(255, 255, 255, 0.2); /* Transparência */
-            backdrop-filter: blur(10px); /* Efeito de desfoque */
-            color: white;
-            padding: 20px;
-            border-radius: 10px;
-            display: none; /* Inicialmente invisível */
-        }
 
-        /* Posicionamento dos painéis em duas linhas de 3 colunas */
-        .panel-1 { top: 10%; left: 5%; }
-        .panel-2 { top: 10%; left: 35%; }
-        .panel-3 { top: 10%; left: 65%; }
-        .panel-4 { top: 60%; left: 5%; }
-        .panel-5 { top: 60%; left: 35%; }
-        .panel-6 { top: 60%; left: 65%; }
+                <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0" material="" geometry=""></a-plane>
 
-        /* Texto dos painéis */
-        .glass-panel h2 {
-            font-size: 1.5rem;
-            margin-bottom: 10px;
-        }
-
-        .glass-panel p {
-            font-size: 1.2rem;
-        }
-
-        .clickable {
-            cursor: pointer;
-        }
-    </style>
-</head>
-<body>
-
-<div class="example-container">
-    <div id="example-scanning-overlay">
-        <!-- A linha de escaneamento (animação) -->
-        <div class="scanline"></div>
-        <div class="inner">
-            <img src="/images/mtg/custom_images/Magic_card_back.png" alt="Card Back">
+                <a-entity visible="false" id="portfolio-panel" position="0 0 -0.01">
+                    <a-text value="Cloudpost" color="black" align="center" width="2" position="0 0.4 0" text=""></a-text>
+                    <a-entity id="portfolio-item0">
+                        <a-video id="paintandquest-video-link" webkit-playsinline="" playsinline="" width="1" height="0.552" position="0 0 0" material="" geometry=""></a-video>
+                        <a-image id="paintandquest-preview-button" class="clickable" src="#paintandquest-preview" alpha-test="0.5" position="0 0 0" height="0.552" width="1" material="" geometry="">
+                        </a-image>
+                    </a-entity>
+                    <a-entity id="portfolio-item1" visible="false">
+                        <a-image class="clickable" src="#coffeemachine-preview" alpha-test="0.5" position="0 0 0" height="0.552" width="1" material="" geometry=""> </a-image>
+                    </a-entity>
+                    <a-entity id="portfolio-item2" visible="false">
+                        <a-image class="clickable" src="#peak-preview" alpha-test="0.5" position="0 0 0" height="0.552" width="1" material="" geometry=""> </a-image>
+                    </a-entity>
+                    <a-image visible="false" id="portfolio-left-button" class="clickable" src="#icon-left" position="-0.7 0 0" height="0.15" width="0.15" material="" geometry=""></a-image>
+                    <a-image visible="false" id="portfolio-right-button" class="clickable" src="#icon-right" position="0.7 0 0" height="0.15" width="0.15" material="" geometry=""></a-image>
+                </a-entity>
+                <a-entity id="info-panel" position="0 -0.3 0">
+                    <a-plane color="black" opacity="0.8">
+                        <a-text value="Cloudpost" color="white" position="0 0.3 0" scale="1 1 1"></a-text>
+                        <a-text value="Mana Cost: {C}" color="white" position="0 0.2 0" scale="1 1 1"></a-text>
+                        <a-text value="Color: Colorless" color="white" position="0 0.1 0" scale="1 1 1"></a-text>
+                        <a-text value="Text: Add {C} for each Cloudpost" color="white" position="0 0 0" scale="1 1 1"></a-text>
+                        <a-text value="Flavor Text: 'A testament to the greatness of Mirrodin.'" color="white" position="0 -0.1 0" scale="1 1 1"></a-text>
+                        <a-text value="Block & Set: Mirrodin" color="white" position="0 -0.2 0" scale="1 1 1"></a-text>
+                        <a-text value="Price: $10.50" color="white" position="0 -0.3 0" scale="1 1 1"></a-text>
+                        <a-text value="Legal in: Legacy, Vintage" color="white" position="0 -0.4 0" scale="1 1 1"></a-text>
+                        <a-text value="Suggested Decks: Ramp, Colorless Artifact" color="white" position="0 -0.5 0" scale="1 1 1"></a-text>
+                    </a-plane>
+                </a-entity>
+                <a-image visible="false" id="profile-button" class="clickable" src="#icon-profile" position="-0.42 -0.5 0" height="0.15" width="0.15" animation="property: scale; to: 1.2 1.2 1.2; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" material="" geometry=""></a-image>
+                <a-image visible="false" id="web-button" class="clickable" src="#icon-web" alpha-test="0.5" position="-0.14 -0.5 0" height="0.15" width="0.15" animation="property: scale; to: 1.2 1.2 1.2; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" material="" geometry=""></a-image>
+                <a-image visible="false" id="email-button" class="clickable" src="#icon-email" position="0.14 -0.5 0" height="0.15" width="0.15" animation="property: scale; to: 1.2 1.2 1.2; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" material="" geometry=""></a-image>
+                <a-image visible="false" id="location-button" class="clickable" src="#icon-location" position="0.42 -0.5 0" height="0.15" width="0.15" animation="property: scale; to: 1.2 1.2 1.2; dur: 1000; easing: easeInOutQuad; loop: true; dir: alternate" material="" geometry=""></a-image>
+                <a-gltf-model id="avatar" rotation="0 0 0" position="0 -0.25 0" scale="0.004 0.004 0.004" src="#avatarModel" gltf-model=""></a-gltf-model>
+                <a-text id="text" class="clickable" value="" color="black" align="center" width="2" position="0 -1 0" geometry="primitive:plane; height: 0.1; width: 2;" material="opacity: 0.5" text=""></a-text>
+                </a-entity>
+                <canvas class="a-canvas" data-aframe-canvas="true" data-engine="three.js r147" width="1920" height="955"></canvas>
+                <div class="a-loader-title" style="display: none;"></div>
+                <a-entity light="" data-aframe-default-light="" aframe-injected=""></a-entity>
+                <a-entity light="" position="" data-aframe-default-light="" aframe-injected=""></a-entity>
+            </a-scene>
+            <video autoplay="" muted="" playsinline="" style="position: absolute; top: -242.5px; left: 0px; z-index: -2; width: 1920px; height: 1440px;" width="640" height="480"></video>
         </div>
-    </div>
-
-    <!-- MindAR Scene -->
-    <a-scene mindar-image="imageTargetSrc: /images/mtg/minds/cloudpost.mind; uiScanning: #example-scanning-overlay;" embedded="" color-space="sRGB">
-        <!-- Assets -->
-        <a-assets>
-            <img id="card" src="/images/mtg/cards/cloudpost.png">
-            <img id="icon-info" src="/images/icons/info.png">
-        </a-assets>
-
-        <!-- Camera -->
-        <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
-
-        <!-- Carta -->
-        <a-entity mindar-image-target="targetIndex: 0" id="cloudpost-target">
-            <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0"></a-plane>
-
-            <!-- Texto dinâmico (Nome da carta) -->
-            <a-text value="Cloudpost" color="black" align="center" position="0 0.4 0" scale="2 2 2"></a-text>
-        </a-entity>
-
-        <!-- Painéis Glass UI (Estáticos) -->
-        <div id="panel-1" class="glass-panel panel-1">
-            <h2>Cloudpost</h2>
-            <p>Mana Cost: {C}</p>
-            <p>Color: Colorless</p>
+        <div class="mindar-ui-overlay mindar-ui-loading hidden">
+            <div class="loader"></div>
         </div>
-        <div id="panel-2" class="glass-panel panel-2">
-            <h2>Text</h2>
-            <p>Add {C} for each Cloudpost you control.</p>
-        </div>
-        <div id="panel-3" class="glass-panel panel-3">
-            <h2>Flavor Text</h2>
-            <p>"A testament to the greatness of Mirrodin."</p>
-        </div>
-        <div id="panel-4" class="glass-panel panel-4">
-            <h2>Price</h2>
-            <p>$10.50</p>
-        </div>
-        <div id="panel-5" class="glass-panel panel-5">
-            <h2>Legal in</h2>
-            <p>Legacy, Vintage</p>
-        </div>
-        <div id="panel-6" class="glass-panel panel-6">
-            <h2>Suggested Decks</h2>
-            <p>Ramp, Colorless Artifact</p>
-        </div>
-    </a-scene>
-</div>
-
-<script>
-    // Mostrar os painéis quando a carta for detectada
-    const targetEntity = document.getElementById('cloudpost-target');
-    const panels = document.querySelectorAll('.glass-panel');
-
-    targetEntity.addEventListener('mindar-image-target-detected', function() {
-        // Exibir os painéis quando a carta for detectada
-        panels.forEach(panel => panel.style.display = 'block');
-    });
-
-    targetEntity.addEventListener('mindar-image-target-lost', function() {
-        // Ocultar os painéis quando a carta for perdida
-        panels.forEach(panel => panel.style.display = 'none');
-    });
-</script>
-
-</body>
+    </body>
 </html>
