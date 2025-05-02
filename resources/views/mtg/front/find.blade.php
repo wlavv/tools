905577eb85466f671200ac51d3ec004a4f5fa4e9
Bruno <wl_avv@hotmail.com>
2025-05-02 12:53:18 +0100

    Update find.blade.php

--- resources/views/mtg/front/find.blade.php
+++ resources/views/mtg/front/find.blade.php
@@ -3,9 +3,7 @@
 <head>
   <meta charset="utf-8">
   <title>tracking.js - face with camera</title>
-  <link rel="stylesheet" href="assets/demo.css">
-
-  <script src="/build/assets/tracking/tracking-min.js"></script>
+  <script src="/build/assets/tracking/tracking.js"></script>
   <script src="/build/assets/tracking/face-min.js"></script>
    <script src="/node_modules/dat.gui/build/dat.gui.min.js"></script>
   <script src="/build/assets/tracking/stats.min.js"></script>
