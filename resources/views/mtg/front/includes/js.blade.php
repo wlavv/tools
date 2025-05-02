4ca290254385cbcf93784d7655f01ded048ed18b
Bruno <wl_avv@hotmail.com>
2025-05-02 11:59:44 +0100

    Update js.blade.php

--- resources/views/mtg/front/includes/js.blade.php
+++ resources/views/mtg/front/includes/js.blade.php
@@ -1,4 +1,4 @@
-<script async src="https://cdn.jsdelivr.net/npm/opencv.js@1.2.1/opencv.min.js" onload="openCvReady()"></script>
+<script async src="/build/assets/opencv.min.js" onload="openCvReady()"></script>
 
     <script>
         let video = document.getElementById('video');
