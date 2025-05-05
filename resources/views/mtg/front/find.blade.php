30deeeb9b355cf214677e0d7efd7c4e148b6b9da
Bruno <wl_avv@hotmail.com>
2025-05-05 20:13:11 +0100

    Onçy info

--- resources/views/mtg/front/find.blade.php
+++ resources/views/mtg/front/find.blade.php
@@ -17,46 +17,6 @@
             </div>
 
             <a-scene mindar-image="imageTargetSrc: /images/mtg/minds/cloudpost.mind; showStats: false; uiScanning: #example-scanning-overlay; persistent: true;" embedded="" color-space="sRGB" renderer="colorManagement: true, physicallyCorrectLights" vr-mode-ui="enabled: true" device-orientation-permission-ui="enabled: false" inspector="" keyboard-shortcuts="" screenshot="">
-                <a-assets>
-                    <img id="card" src="./assets/card-example/card.png">
-                    <img id="icon-web" src="./assets/card-example/icons/web.png">
-                    <img id="icon-location" src="./assets/card-example/icons/location.png">
-                    <img id="icon-profile" src="./assets/card-example/icons/profile.png">
-                    <img id="icon-phone" src="./assets/card-example/icons/phone.png">
-                    <img id="icon-email" src="./assets/card-example/icons/email.png">
-                    <img id="icon-play" src="./assets/card-example/icons/play.png">
-                    <img id="icon-left" src="./assets/card-example/icons/left.png">
-                    <img id="icon-right" src="./assets/card-example/icons/right.png">
-                    <img id="paintandquest-preview" src="./assets/card-example/portfolio/paintandquest-preview.png">
-                    <video id="paintandquest-video-mp4" autoplay="false" loop="true" src="./assets/card-example/portfolio/paintandquest.mp4" playsinline="" webkit-playsinline=""></video>
-                    <video id="paintandquest-video-webm" autoplay="false" loop="true" src="./assets/card-example/portfolio/paintandquest.webm" playsinline="" webkit-playsinline=""></video>
-                    <img id="coffeemachine-preview" src="./assets/card-example/portfolio/coffeemachine-preview.png">
-                    <img id="peak-preview" src="./assets/card-example/portfolio/peak-preview.png">
-                    <a-asset-item id="avatarModel" src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.2/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item>
-                </a-assets>
-                <a-camera position="0 0 0" look-controls="enabled: false" cursor="fuse: false; rayOrigin: mouse;" raycaster="far: 10000; objects: .clickable" camera="" rotation="" wasd-controls=""></a-camera>
-                                
-                <a-entity id="mytarget" mytarget="" mindar-image-target="targetIndex: 0">
-
-
-                <a-plane src="#card" position="0 0 0" height="0.552" width="1" rotation="0 0 0" material="" geometry=""></a-plane>
-
-                <a-entity visible="false" id="portfolio-panel" position="0 0 -0.01">
-                    <a-text value="Cloudpost" color="black" align="center" width="2" position="0 0.4 0" text=""></a-text>
-                    <a-entity id="portfolio-item0">
-                        <a-video id="paintandquest-video-link" webkit-playsinline="" playsinline="" width="1" height="0.552" position="0 0 0" material="" geometry=""></a-video>
-                        <a-image id="paintandquest-preview-button" class="clickable" src="#paintandquest-preview" alpha-test="0.5" position="0 0 0" height="0.552" width="1" material="" geometry="">
-                        </a-image>
-                    </a-entity>
-                    <a-entity id="portfolio-item1" visible="false">
-                        <a-image class="clickable" src="#coffeemachine-preview" alpha-test="0.5" position="0 0 0" height="0.552" width="1" material="" geometry=""> </a-image>
-                    </a-entity>
-                    <a-entity id="portfolio-item2" visible="false">
-                        <a-image class="clickable" src="#peak-preview" alpha-test="0.5" position="0 0 0" height="0.552" width="1" material="" geometry=""> </a-image>
-                    </a-entity>
-                    <a-image visible="false" id="portfolio-left-button" class="clickable" src="#icon-left" position="-0.7 0 0" height="0.15" width="0.15" material="" geometry=""></a-image>
-                    <a-image visible="false" id="portfolio-right-button" class="clickable" src="#icon-right" position="0.7 0 0" height="0.15" width="0.15" material="" geometry=""></a-image>
-                </a-entity>
                 <a-entity id="info-panel" position="0 -0.3 0">
                     <a-plane color="black" opacity="0.8">
                         <a-text value="Cloudpost" color="white" position="0 0.3 0" scale="1 1 1"></a-text>
