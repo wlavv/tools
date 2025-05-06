fe9822f1291302f40f9d6758d26622f51bd4916b
Bruno <wl_avv@hotmail.com>
2025-05-06 21:43:09 +0100

    Update AR_content.blade.php

--- resources/views/mtg/front/includes/AR_content.blade.php
+++ resources/views/mtg/front/includes/AR_content.blade.php
@@ -15,14 +15,6 @@
         </div>
         <div class="panel">
             <h2 class="panel_title">KNOWN DECKS</h2>
-            <p>Mana Cost: {C}</p>
-            <p>Color: Colorless</p>
-            <p>Text: Add {C} for each Cloudpost you control.</p>
-            <p>Flavor Text: "A testament to the greatness of Mirrodin."</p>
-            <p>Block & Set: Mirrodin</p>
-            <p>Price: $10.50</p>
-            <p>Legal in: Legacy, Vintage</p>
-            <p>Suggested Decks: Ramp, Colorless Artifact</p>
         </div>
     </div>
     <div class="panel_parent">
@@ -33,22 +25,10 @@
     </div>
     <div class="panel_parent">
         <div class="panel">
-            <h2 class="panel_title">ARTICLES</h2>
-            <p>Exemplo de conteúdo adicional:</p>
-            <ul>
-                <li>Estatísticas do Deck</li>
-                <li>Cartas semelhantes</li>
-                <li>Links para compra</li>
-            </ul>
+            <h2 class="panel_title">ARTICLES</h2>            
         </div>
         <div class="panel">
-            <h2 class="panel_title">OFFICIAL INFO</h2>
-            <p>Exemplo de conteúdo adicional:</p>
-            <ul>
-                <li>Estatísticas do Deck</li>
-                <li>Cartas semelhantes</li>
-                <li>Links para compra</li>
-            </ul>
+            <h2 class="panel_title">OFFICIAL INFO</h2>            
         </div>
     </div>
 </div>
