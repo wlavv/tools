580dacb6954bb19a428ed56b6b1a1c3955a787a0
Bruno <wl_avv@hotmail.com>
2025-05-17 10:09:07 +0100

    Update AR_content.blade.php

--- resources/views/mtg/front/includes/AR_content.blade.php
+++ resources/views/mtg/front/includes/AR_content.blade.php
@@ -105,10 +105,10 @@
                         <div style="width: calc( 100% - 60px); float: left;padding: 11px 0;"> <span style="padding-left: 5px;color: #FFF;">VIDEOS</span> </div>
                     </a>
                 </div>
-                <div style="text-align: center;">
+                <div style="text-align: left;">
                     <a href="https://www.reddit.com/r/EDH/search/?q={{urlencode($card->name)}}" target="_blank" title="{{$card->name}}" style="verticel-align: middle;text-transform: uppercase;">
                         <div style="width: 60px; float: left;">
-                            <img src="/images/mtg/custom_images/reddit.png" alt="MTG WIKI" style="width: 40px;">
+                            <img src="/images/mtg/custom_images/reddit.png" alt="MTG WIKI" style="width: 30px;">
                         </div>
                         <div style="width: calc( 100% - 60px); float: left;padding: 11px 0;"> <span style="padding-left: 5px;color: #FFF;">REDDIT - {{$card->name}}</span> </div>
                     </a>
@@ -118,7 +118,7 @@
         <div class="panel">
             <h2 class="panel_title">OFFICIAL INFO</h2>  
             <div>
-                <div style="float: left;text-align: center;">
+                <div style="text-align: center;">
                     <a href="https://mtg.wiki/index.php?title={{str_replace(' ', '_', $card->name)}}&redirect=yes" target="_blank" title="{{$card->name}}" style="verticel-align: middle;text-transform: uppercase;">
                         <div style="width: 60px; float: left;">
                             <img src="/images/mtg/custom_images/mtgwiki.png" alt="MTG WIKI" style="width: 40px;">
