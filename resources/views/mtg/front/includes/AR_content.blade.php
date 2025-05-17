0562420d42a18754fde79003f487149a696e46c9
Bruno <wl_avv@hotmail.com>
2025-05-17 15:20:13 +0100

    Update AR_content.blade.php

--- resources/views/mtg/front/includes/AR_content.blade.php
+++ resources/views/mtg/front/includes/AR_content.blade.php
@@ -71,7 +71,6 @@
     </div>
     <div class="panel_parent">
         <div class="panel">
-            <h2 class="panel_title" style="text-align: center;">{{$card->name}}</h2>
             <div class="cardCenterContainer" id="cardImageContainer">
                 <img src="{{$card->image_url}}" style="max-width: 100%;margin: 0 auto;border-radius: 25px; border: 1px solid #333; box-shadow: 0 4px 12px rgba(0, 0, 0, 0.4), 0 8px 24px rgba(0, 0, 0, 0.25);">
             </div>
@@ -81,10 +80,13 @@
             <div class="cardCenterContainer" id="cardBanningContainer">
                 banning
             </div>
-            <div style="margin: 20px 0px;">
+            <div style="margin: 20px;">
                 <div onclick="$('.cardCenterContainer').css('display', 'none'); $('#cardImageContainer').css('display', 'block');" style="width: 33%; float: left; text-align: center; font-size: 26px; background-color: #FFF; color: #333;border: 1px solid #FFF;padding: 10px 0;"><i class="fa-solid fa-image"></i></div>
                 <div onclick="$('.cardCenterContainer').css('display', 'none'); $('#cardPriceContainer').css('display', 'block');" style="width: 32%; float: left; text-align: center; font-size: 26px; background-color: transparent; color: #fff;border: 1px solid #FFF;padding: 10px 0;"><i class="fa-solid fa-euro-sign"></i></div>
                 <div onclick="$('.cardCenterContainer').css('display', 'none'); $('#cardBanningContainer').css('display', 'block');" style="width: 33%; float: left; text-align: center; font-size: 26px; background-color: transparent; color: #fff;border: 1px solid #FFF;padding: 10px 0;"><i class="fa-solid fa-ban"></i></div>
+            </div>
+            <div style="margin: 20px 0;">
+                <h2 class="panel_title" style="text-align: center;">{{$card->name}}</h2>
             </div>
         </div>
     </div>
