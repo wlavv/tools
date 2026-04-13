cb7e9d605a6283136a9654b60a426e6db9c2c68e
Bruno <wl_avv@hotmail.com>
2026-04-13 22:21:15 +0100

    Update tablet.blade.php

--- Modules/Tasks/Resources/views/layouts/tablet.blade.php
+++ Modules/Tasks/Resources/views/layouts/tablet.blade.php
@@ -15,9 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <style>
-        @include('tasks::includes.tablet_css')
-    </style>
+    @include('tasks::includes.tablet_css')
 </head>
 <body class="tablet-body family-planner-tablet">
     @yield('content')
