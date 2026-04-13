37f3b9b7f534fcf9c379327aa9468a8134dfbf7f
Bruno <wl_avv@hotmail.com>
2026-04-13 22:14:41 +0100

    Update tablet.blade.php

--- Modules/Tasks/Resources/views/layouts/tablet.blade.php
+++ Modules/Tasks/Resources/views/layouts/tablet.blade.php
@@ -2,8 +2,12 @@
 <html lang="pt">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="Family Planner">
+
     <title>Family Planner</title>
     
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
