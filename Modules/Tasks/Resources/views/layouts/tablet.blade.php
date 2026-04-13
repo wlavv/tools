4667d22cac3ba4d493b4cd349115cc86cea7d260
Bruno <wl_avv@hotmail.com>
2026-04-13 22:17:30 +0100

    Update tablet.blade.php

--- Modules/Tasks/Resources/views/layouts/tablet.blade.php
+++ Modules/Tasks/Resources/views/layouts/tablet.blade.php
@@ -9,16 +9,20 @@
     <meta name="apple-mobile-web-app-title" content="Family Planner">
 
     <title>Family Planner</title>
-    
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
-    @include('tasks::includes.tablet_css')
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <style>
+        @include('tasks::includes.tablet_css')
+    </style>
 </head>
 <body class="tablet-body family-planner-tablet">
     @yield('content')
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     @include('tasks::includes.tablet_js')
 </body>
 </html>
